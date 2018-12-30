b6e52ccc478010e930728fe8ac75b18bd31fa235
John Ray <john@johnraymon.com>
2018-12-30 12:50:43 -0500

    add tracking update copy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match "/404", :to => "error#not_found", :via => :all
   post '/subscribe', to: 'pages#subscribe'
   get "/", to: 'pages#splash'
   # get "/*all", to: 'pages#splash'
